ef76d0524dcdd01457e4f3c4f4a6ef82772e3f6a
Drake <drake11weaver@gmail.com>
2018-11-01 19:27:45 -0500

    Sprites memory is cleared upon termination

--- Attack_of_pi_menu_gui.py
+++ Attack_of_pi_menu_gui.py
@@ -16,17 +16,14 @@ def read_high_score():
 		return data
 
 
-pygame.font.init()
-myfont = pygame.font.SysFont('Comic Sans MS', 20)
 highscore = read_high_score()
 
-#Sprites.init()
-FPS = 60
-class Game(Frame):
-		def __init__(self,master, score, lives, waves):
+
+class Menu(Frame):
+		def __init__(self,master):
 				Frame.__init__(self,master)
 
-				self.startbutton = Button(master, text = "START", fg = "white",bg = "green",  command = self.play, height = 2)
+				self.startbutton = Button(master, text = "START", fg = "white",bg = "green",  command = self.CreateGame, height = 2)
 				self.startbutton.pack(side = TOP,fill = X)
 
 				self.button2 = Button(master, text = "QUIT", fg = "white",bg = "red", command = self.quit, height = 2)
@@ -36,161 +33,169 @@ class Game(Frame):
 
 				self.l = Label(master, image = self.img)
 				self.l.pack(side = BOTTOM, fill=X)
-				self.score = score
-				self.lives = lives
-				self.waves = waves
-				self.dead = False
+	
+		def CreateGame(self):
+			Game(score= 0, lives= 5, waves = 1)
 
-		@property
-		def score(self):
-				return self._score
+				
 
-		@score.setter
-		def score(self, value):
-				self._score = value
+class Game():
+	def __init__(self, score, lives, waves):
+		pygame.init()
+		self.background = pygame.image.load('background_image.gif')
+		self.background_size = self.background.get_size()
+		self.background_rect = self.background.get_rect()
+		self.screen = pygame.display.set_mode(self.background_size)
+		self.score = score
+		self.lives = lives
+		self.waves = waves
+		self.running = True
+		self.dead = False
+		self.play()
 
-		@property
-		def lives(self):
-				return self._lives
-		
-		@lives.setter
-		def lives(self, value):
-				self._lives = value
+	@property
+	def score(self):
+			return self._score
 
-		@property
-		def waves(self):
-				return self._waves
-		
-		@waves.setter
-		def waves(self, value):
-				self._waves = value
+	@score.setter
+	def score(self, value):
+			self._score = value
+
+	@property
+	def lives(self):
+			return self._lives
+	
+	@lives.setter
+	def lives(self, value):
+			self._lives = value
+
+	@property
+	def waves(self):
+			return self._waves
+	
+	@waves.setter
+	def waves(self, value):
+			self._waves = value
 
 
-		def events(self, Console):
-				if (Console == "PC"):
-						keys = pygame.key.get_pressed()
+	def events(self, Console):
+			if (Console == "PC"):
+					keys = pygame.key.get_pressed()
 
-						if keys[K_RIGHT]:
-								self.player.xVel = 5
-								
-						elif keys[K_LEFT]:
-								self.player.xVel = -5
+					if keys[K_RIGHT]:
+							self.player.xVel = 5
+							
+					elif keys[K_LEFT]:
+							self.player.xVel = -5
 
-						else:
-								self.player.xVel = 0
-								self.player.yVel = 0
+					else:
+							self.player.xVel = 0
+							self.player.yVel = 0
 
-						for event in pygame.event.get():
-								if event.type == pygame.QUIT:
-										global running
-										global highscore
-										running = False
-										if (int(highscore) < self.score):
-											self.high_score(self.score)  
-								if event.type == pygame.KEYDOWN:
-										if event.key==pygame.K_SPACE:
-											if self.dead == False:
-												Sprites.Bullet(self.player, screen)
-										if event.key ==pygame.K_e:
-												Sprites.Enemy1(300, 50, screen)
-				elif (Console == "PI"):
-						mstatus = Joystick.readChannel(1)
-						fstatus = Joystick.readChannel(0)
-						pygame.event.pump()									
-						if fstatus == "YES":
-							if self.dead == False:
-								Sprites.Bullet(self.player, screen)
+					for event in pygame.event.get():
+							if event.type == pygame.QUIT:
+									global highscore
+									self.running = False
+									if (int(highscore) < self.score):
+										self.high_score(self.score)
+							if event.type == pygame.KEYDOWN:
+									if event.key==pygame.K_SPACE:
+										if self.dead == False:
+											Sprites.Bullet(self.player, self.screen)
+									if event.key ==pygame.K_e:
+											Sprites.Enemy1(300, 50, self.screen)
+			elif (Console == "PI"):
+					mstatus = Joystick.readChannel(1)
+					fstatus = Joystick.readChannel(0)
+					pygame.event.pump()									
+					if fstatus == "YES":
+						if self.dead == False:
+							Sprites.Bullet(self.player, self.screen)
 
-						if mstatus == "Right":
-								self.player.xVel = 1
-								
-						elif mstatus == "Left":
-								self.player.xVel = -1
+					if mstatus == "Right":
+							self.player.xVel = 1
+							
+					elif mstatus == "Left":
+							self.player.xVel = -1
 
-						else:
-								self.player.xVel = 0
-								self.player.yVel = 0
+					else:
+							self.player.xVel = 0
+							self.player.yVel = 0
 
-						for event in pygame.event.get():
-								if event.type == pygame.QUIT: 
-										global running
-										running = False
-		
+					for event in pygame.event.get():
+							if event.type == pygame.QUIT: 
+									self.running = False
+	
 
 
 
 
 #starts the game
-		def play(self):
-				theClock = pygame.time.Clock()
-				background = pygame.image.load('background_image.gif')
-				background_size = background.get_size()
-				background_rect = background.get_rect()
-				global screen
-				screen = pygame.display.set_mode(background_size)
-				w,h = background_size
-				x = 0
-				y = 0
-				x1 = 0
-				y1 = -h
-				#Sprites.init()
-				self.player = Sprites.Player(250,430, screen)
-				global running
-				global Console
-				running = True
-				while running:
-					#screen.blit(background,background_rect) ---- When the FPS was ramped up this was not needed and actually was the cause of our screen tearing
-					pygame.display.update()
-					y1 += 5
-					y += 5
-					screen.blit(background,(x,y))
-					screen.blit(background,(x1,y1))
-					self.score_counter(screen, self.score)
-					self.life_counter(screen, self.lives)
-					self.events(Console)
-					Sprites.GenLevel(screen, self.waves)
-					self.events(Console)
-					Sprites.players.update()
-					Sprites.bullets.update()
-					Sprites.enemys.update()
-					Sprites.enemyBullets.update()
-					if Sprites.enemyDeath():
-						self.score = self.score + 5
-						print "enemy died"
-					if Sprites.playerDeath():
-						if self.lives > 0:
-							self.lives = self.lives - 1
-							self.player = Sprites.Player(250,430, screen)
-							print "player died"
-						else:
-							self.dead = True
-							print "GAME OVER"
-					if y > h:
-						y = -h
-					if y1 > h:
-						y1 = -h
-					pygame.display.flip()
-					pygame.display.update()
-					theClock.tick(FPS)
-				pygame.quit()
+	def play(self):
+			pygame.font.init()
+			theClock = pygame.time.Clock()
+			w,h = self.background_size
+			x = 0
+			y = 0
+			x1 = 0
+			y1 = -h
+			#Sprites.init()
+			self.player = Sprites.Player(250,430, self.screen)
+			global Console
+			self.running = True
+			while self.running:
+				#screen.blit(background,background_rect) ---- When the FPS was ramped up this was not needed and actually was the cause of our screen tearing
+				pygame.display.update()
+				y1 += 5
+				y += 5
+				self.screen.blit(self.background,(x,y))
+				self.screen.blit(self.background,(x1,y1))
+				self.score_counter(self.screen, self.score)
+				self.life_counter(self.screen, self.lives)
+				self.events(Console)
+				Sprites.GenLevel(self.screen, self.waves)
+				self.events(Console)
+				Sprites.players.update()
+				Sprites.bullets.update()
+				Sprites.enemys.update()
+				Sprites.enemyBullets.update()
+				if Sprites.enemyDeath():
+					#happens when enemy dies
+					self.score = self.score + 5
+				if Sprites.playerDeath():
+					if self.lives > 0:
+						#happens when player dies but the game is not over
+						self.lives = self.lives - 1
+						self.player = Sprites.Player(250,430, self.screen)
+					else:
+						#happens when game is over
+						self.dead = True
+						
+				if y > h:
+					y = -h
+				if y1 > h:
+					y1 = -h
+				pygame.display.flip()
+				pygame.display.update()
+				theClock.tick(60)
+			Sprites.killAll()
+			pygame.display.quit()
+			pygame.quit()
+	def score_counter(self, screen, score):
+			myfont = pygame.font.SysFont('Comic Sans MS', 20)
+			textsurface = myfont.render("Score = {}".format(score), False, (255, 255, 255))
+			screen.blit(textsurface,(0,420))
 
-		def score_counter(self, screen, score):
-				textsurface = myfont.render("Score = {}".format(score), False, (255, 255, 255))
-				screen.blit(textsurface,(0,420))
-
-		def life_counter(self, screen, lives):
-				textsurface = myfont.render("Lives = {}".format(lives), False, (255, 255, 255))
-				screen.blit(textsurface,(700,420))
-				
-		def high_score(self, score):
-				s = open("score.txt","w+")#this opens up the file 
-				s.write(str(score))
-				s.close()
- 
-		
-#ends and exits the game
-		def quit(self):
-				self.master.destroy()
+	def life_counter(self, screen, lives):
+			myfont = pygame.font.SysFont('Comic Sans MS', 20)
+			textsurface = myfont.render("Lives = {}".format(lives), False, (255, 255, 255))
+			screen.blit(textsurface,(700,420))
+			
+	def high_score(self, score):
+			myfont = pygame.font.SysFont('Comic Sans MS', 20)
+			s = open("score.txt","w+")#this opens up the file 
+			s.write(str(score))
+			s.close()
 
 
 
@@ -204,7 +209,7 @@ HEIGHT = 550
 window = Tk()
 window.geometry("{}x{}".format(WIDTH,HEIGHT))
 window.title("Attack of The Pi !")
-menu = Game(window, score= 0, lives= 5, waves = 1)
+menu = Menu(window)
 window.mainloop()
 
 

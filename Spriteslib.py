0ee2efeb45b5170c4106b08d8678c3b91751ff27
mlevesque2021 <43627726+mlevesque2021@users.noreply.github.com>
2018-11-05 19:51:20 -0600

    Pull REquest didnt work properly so had to merge manually

--- Spriteslib.py
+++ Spriteslib.py
@@ -1,16 +1,25 @@
-import math, random, sys
+import random
 import pygame
 from pygame.locals import *
+from random import randint
+from pygame import mixer
 
-def init():
-	pygame.init()
-	FPS = 60
-	current_Frame = 0
-	#define some groups
-	bullets = pygame.sprite.Group()
-	enemys = pygame.sprite.Group()
-	player = Player(300,270)
-	CENTER_HANDLE = 4
+CENTER_HANDLE = 4
+pygame.init()
+mixer.init()
+#define some groups
+
+
+bullets = pygame.sprite.Group()
+enemyBullets = pygame.sprite.Group()
+enemys = pygame.sprite.Group()
+players = pygame.sprite.Group()
+beams = pygame.sprite.Group()
+ships = pygame.sprite.Group()
+xPos = [x * 48 for x in range(2,20)]
+yPos = [y * 20 for y in range (1,5)]
+
+
 
 class spritesheet:
 	def __init__(self, filename, cols, rows):
@@ -40,20 +49,48 @@ class spritesheet:
 
 class Player(pygame.sprite.Sprite):
 
-	def __init__(self, x, y):
+	def __init__(self, x, y, screen):
 		pygame.sprite.Sprite.__init__(self)
+		players.add(self)
+		self.ChannelF = pygame.mixer.Channel(5)
+		self.fighter_captured = pygame.mixer.Sound("resources/Sounds/fighter_captured.ogg")
+		self.screen = screen
 		self.x = x
 		self.y = y
 		self.xVel = 0
 		self.yVel = 0
-		self.spritesheet = spritesheet("Sprites/player_Spritesheet.png",8,1)
-		
+		self.linked = None
+		self.isLinked = 0
+		self.spritesheet = spritesheet("resources/Sprites/player_Spritesheet.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/player collison.png")
+		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
 		self.index = 7
 		
+	def linkPlayer(self):
+		self.linked = Player2(self)
+		self.isLinked = 1
+
+	def shoot(self):
+		Bullet(self, self.screen)
+		if self.isLinked == 1:
+			Bullet(self.linked, self.screen)
+		
 	def update(self):
+		if (self.x > 800):
+			self.x = 150
+		elif (self.x < 150):
+			self.x = 800
+		if pygame.sprite.spritecollideany(self, beams, pygame.sprite.collide_mask):
+			Captured(self.screen, self)
+			self.ChannelF.play(self.fighter_captured)
+			self.kill()
+		if pygame.sprite.spritecollideany(self, enemyBullets, pygame.sprite.collide_mask):
+			self.kill()
 		self.x = self.x + self.xVel
 		self.y = self.y + self.yVel
-		self.spritesheet.draw(screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		self.rect.center = ((self.x,self.y))
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
 		
 	@property
 	def xVel(self):
@@ -61,7 +98,7 @@ class Player(pygame.sprite.Sprite):
 	
 	@xVel.setter
 	def xVel(self, value):
-		self._xVel = value
+		self._xVel = value 
 		
 	@property
 	def yVel(self):
@@ -71,83 +108,349 @@ class Player(pygame.sprite.Sprite):
 	def yVel(self, value):
 		self._yVel = value
 
+class Player2(pygame.sprite.Sprite):
+
+	def __init__(self, player):
+		pygame.sprite.Sprite.__init__(self)
+		players.add(self)
+		self.x = player.x + 15
+		self.y = player.y
+		self.player = player
+		self.screen = player.screen
+		self.spritesheet = spritesheet("resources/Sprites/player_Spritesheet.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/player collison.png")
+		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
+<<<<<<< HEAD
+		self.spritesheet = spritesheet("resources/Sprites/player_Spritesheet.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/player collison.png")
+		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
+=======
+>>>>>>> cc794846d113c8b24da5716f928624f31d9dee47
+		self.index = 7
+
+	def update(self):
+		self.x = self.player.x + 15
+		self.y = self.player.y
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		
 class Enemy(pygame.sprite.Sprite):
 
-	def __init__(self, x, y):
+
+	def __init__(self, x, y, screen, level):
 		pygame.sprite.Sprite.__init__(self)
+		self.ChannelD = pygame.mixer.Channel(3)
+		self.bulletSound = pygame.mixer.Sound("resources/Sounds/laser_widebeam.ogg") 
 		enemys.add(self)
+		self.screen = screen
+		self.level = level
+		self.current_Frame = 0
+		self.time = 0
 		self.x = x
+		self.xStart = 0
 		self.y = y
+		self.yStart = 0
+		self.xVel = 1
+		self.yVel = 0
+		self.spritesheet = spritesheet("resources/Sprites/enemy3.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/enemy collison.png")
+		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
+		self.index = 7
+		self.lock = 1
+		self.beam = None
+
+	@property
+	def xVel(self):
+		return self._xVel
+
+	@xVel.setter
+	def xVel(self, value):
+		self._xVel = value
+
+	@property
+	def yVel(self):
+		return self._yVel
+
+	@yVel.setter
+	def yVel(self, value):
+		self._yVel = value
+
+
+	def idle (self):
+		if ((self.current_Frame % 10) == 0 and self.index == 6):
+			self.index = 7
+		elif ((self.current_Frame % 10) == 0 and self.index == 7):
+			self.index = 6
+
+	def update(self):
+		if ((self.current_Frame % 12) == 0):
+			self.xVel = randint(1,11)-6
+
+		if (self.x > 800):
+			self.x = 150
+		elif (self.x < 150):
+			self.x = 800
+		if self.lock == 1:
+			self.x = self.x + self.xVel
+			self.y = self.y + self.yVel
+
+		self.rect.center = ((self.x,self.y))
+		self.current_Frame = self.current_Frame + 1
+		if ((self.current_Frame % 60) == 0):
+			self.current_Frame = 0
+			self.time = self.time + 1
+
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		self.idle()
+		self.shoot()
+
+	def shoot(self):
+		ran = randint(0,((200-self.level)+100))
+		if ran == 1:
+			self.ChannelD.play(self.bulletSound)
+			EnemyBullet(self, self.screen)
+
+	def mayDrop(self):
+		ran = randint(0,((20-self.level)+2000))
+		if (ran == 7 and self.lock == 1):
+			self.xStart = self.x
+			self.yStart = self.y
+			self.time = 0
+			if self.x < 600:
+				self.dropDown(self.xStart, self.yStart)
+				self.lock = 0
+
+		elif (self.lock == 0 and self.time < 3):
+			self.dropDown(self.xStart, self.yStart)
+			
+		elif self.time == 3 and self.current_Frame == 1 and self.lock == 0:
+
+			self.beam = Beam(self, self.screen)
+	
+		elif self.time > 5 and self.lock == 0:
+			self.beam.killBeam()
+			self.goUp(self.xStart, self.yStart)
+
+	def dropDown(self, x, y):
+		xMove = 0
+		self.xVel = 1
+		if self.y < 350:
+			#print self.x
+			self.x = self.x + self.xVel
+			xMove = self.x - x
+			self.y = 0.02*(xMove ** 2) + y
+			
+	def goUp(self, x, y):
+		xMove = 0
+		self.xVel = -1
+		if self.x > x:
+			self.x = self.x + self.xVel
+			xMove = self.x - x
+			self.y = 0.02*(xMove ** 2) + y
+		else:
+			self.lock = 1
+
+class Enemy1(Enemy):
+	def __init__(self, x, y, screen, level):
+		Enemy.__init__(self, x, y, screen, level)
+		self.spritesheet = spritesheet("resources/Sprites/enemy1.png",8,1)
+
+	def update(self):
+		Enemy.update(self)
+		self.mayDrop()
+		
+
+
+class Enemy2(Enemy):
+	def __init__(self, x, y, screen, level):
+		Enemy.__init__(self, x, y, screen, level)
+		self.spritesheet = spritesheet("resources/Sprites/enemy2.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/collison2.png")
+		
+		
+class Enemy3(Enemy):
+	def __init__(self, x, y, screen, level):
+		Enemy.__init__(self, x, y, screen, level)
+		self.spritesheet = spritesheet("resources/Sprites/enemy3.png",8,1)
+
+	def update(self):
+		Enemy.update(self)
+		self.mayDrop()
+
+		
+class Enemy4(Enemy):
+	def __init__(self, x, y, screen, level):
+		Enemy.__init__(self, x, y, screen, level)		
+		self.spritesheet = spritesheet("resources/Sprites/enemy4.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/collison2.png")		
+	
+class Captured(pygame.sprite.Sprite):
+	def __init__(self, screen, player):
+		pygame.sprite.Sprite.__init__(self)
+		ships.add(self)
+		self.x = player.x
+		self.y = player.y
 		self.xVel = 0
 		self.yVel = 0
-		self.spritesheet = spritesheet("Sprites/enemy3.png",8,1)
-		self.image = pygame.image.load("Sprites/enemy collison.png")
+		self.current_Frame = 0
+		self.screen = screen
+		self.spritesheet = spritesheet("resources/Sprites/Captured.png",8,1)
+		self.image = pygame.image.load("resources/Sprites/player collison.png")
 		self.rect = self.image.get_rect()
 		self.mask = pygame.mask.from_surface(self.image)
 		self.index = 7
 		
-	def idle (self):
-		if ((current_Frame % 10) == 0 and self.index == 6):
-			self.index = 7
-		elif ((current_Frame % 10) == 0 and self.index == 7):
-			self.index = 6
-		
 	def update(self):
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		if self.y > 20 :
+			self.yVel = -3
+		else:
+			self.yVel = 0
+			if ((self.current_Frame % 12) == 0):
+				self.xVel = randint(1,11)-6
+
+		if (self.x > 800):
+			self.x = 150
+		elif (self.x < 150):
+			self.x = 800
+		self.current_Frame = self.current_Frame + 1
+		if self.current_Frame % 60 == 0:
+			self.current_Frame = 0
+		self.rect.center = ((self.x,self.y))
 		self.x = self.x + self.xVel
 		self.y = self.y + self.yVel
-		self.rect.center = ((self.x,self.y))
-		self.spritesheet.draw(screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
-		self.idle()
-		
+		if pygame.sprite.spritecollideany(self, bullets, pygame.sprite.collide_mask):
+			self.kill()
 
 class Bullet(pygame.sprite.Sprite):
-	def __init__(self):
+	def __init__(self, player, screen):
 		pygame.sprite.Sprite.__init__(self)
 		bullets.add(self)
+		self.screen = screen
+		self.player = player
 		self.x = player.x
 		self.y = player.y + 10
 		self.xVel = 0
 		self.yVel = -5
-		self.spritesheet = spritesheet("Sprites/bullet.png",1,1)
-		self.image = pygame.image.load("Sprites/bullet.png")
+		self.spritesheet = spritesheet("resources/Sprites/bullet.png",1,1)
+		self.image = pygame.image.load("resources/Sprites/bullet.png")
 		self.rect = self.image.get_rect()
 		self.mask = pygame.mask.from_surface(self.image)
 		self.index = 1
 		
 	def update(self):
-		self.spritesheet.draw(screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
 		#self.mask.set_at((self.x, self.y))
 		self.x = self.x + self.xVel
 		self.y = self.y + self.yVel
 		self.rect.center = ((self.x,self.y))
+		if pygame.sprite.spritecollideany(self, ships, pygame.sprite.collide_mask):
+			self.kill()
+			self.player.linkPlayer()
+			
 		if self.y < -20:
 			bullets.remove(self)
-class Enemy1(Enemy):
-	def __init__(self, x, y):
-		Enemy.__init__(self, x, y)
+
+
+class EnemyBullet(pygame.sprite.Sprite):
+	def __init__(self, enemy, screen):
+		pygame.sprite.Sprite.__init__(self)
+		enemyBullets.add(self)
+		self.screen = screen
+		self.x = enemy.x
+		self.y = enemy.y - 10
+		self.xVel = 0
+		self.yVel = 5
+		self.spritesheet = spritesheet("resources/Sprites/EnemyBullet.png",1,1)
+		self.image = pygame.image.load("resources/Sprites/bullet.png")
+		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
+		self.index = 1
+		
+	def update(self):
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		self.x = self.x + self.xVel
+		self.y = self.y + self.yVel
+		self.rect.center = ((self.x,self.y))
+		#pygame.sprite.spritecollide(self, players, True, pygame.sprite.collide_mask)
+		if self.y > 500:
+			bullets.remove(self)
+
+class Beam(pygame.sprite.Sprite):
+	def __init__(self, enemy, screen):
+		pygame.sprite.Sprite.__init__(self)
+		beams.add(self)
+		self.ChannelE = pygame.mixer.Channel(4)
+		self.tractorSound = pygame.mixer.Sound("resources/Sounds/tractor_beam1.ogg")
+		self.screen = screen
+		self.x = enemy.x
+		self.y = enemy.y + 50
+		self.current_Frame = 0
+		self.spritesheet = spritesheet("resources/Sprites/beam.png",3,1)
+		self.image = pygame.image.load("resources/Sprites/beam collison.png")
+		self.rect = self.image.get_rect()
+		self.mask = pygame.mask.from_surface(self.image)
+		self.index = 1
 	
-def events():
-	keys = pygame.key.get_pressed()
-	#pygame.event.pump()
-
-	if keys[K_RIGHT]:
-		player.xVel = 1
+	def killBeam(self):
+		beams.remove(self)
+	
+	def update(self):
+		self.spritesheet.draw(self.screen, self.index % self.spritesheet.totalCellCount, self.x, self.y, CENTER_HANDLE)
+		self.current_Frame = self.current_Frame + 1
+		self.rect.center = ((self.x,self.y))
+		self.ChannelE.play(self.tractorSound)
+		if self.current_Frame % 60 == 0:
+			self.current_Frame = 0
+		if self.current_Frame % 20 == 0 :
+			self.index = self.index + 1
+		if self.index % 3 == 0:
+			self.index = 0
+			
+def GenLevel(screen, level):
+	difficuly = (level * 5) + 10
+	if not enemys :
+		for x in range (difficuly):
+			PickEnemy(randint(0,4),xPos[randint(0,9)],yPos[randint(0,3)],screen,level)
+			level =  level + 1
 		
-	elif keys[K_LEFT]:
-		player.xVel = -1
-
+def PickEnemy(i, x, y, screen, level):
+	if i == 0 :
+		Enemy1(x, y, screen, level)
+	if i == 1 :
+		Enemy2(x, y, screen, level)
+	if i == 2 :
+		Enemy3(x, y, screen, level)
+	if i == 3 :
+		Enemy4(x, y, screen, level)
+		
+def enemyDeath():
+	if pygame.sprite.groupcollide(bullets, enemys, True, pygame.sprite.collide_mask):
+		return True
 	else:
-		player.xVel = 0
-		player.yVel = 0
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: 
-			sys.exit()	
-		if event.type == pygame.KEYDOWN:
-			if event.key==pygame.K_SPACE:
-				Bullet()
-			if event.key ==pygame.K_e:
-				Enemy1(HW, 50)
-		
-# main loop
-
+		return False
+	
+def playerDeath():
+<<<<<<< HEAD
+	if pygame.sprite.groupcollide(enemyBullets, players, True, pygame.sprite.collide_mask):
+		playerDied = pygame.mixer.Sound("resources/Sounds/fighter_destroyed.ogg")
+		ChannelG = pygame.mixer.Channel(6)
+		ChannelG.play(playerDied)
+=======
+	if pygame.sprite.groupcollide(enemyBullets, players, False, False, pygame.sprite.collide_mask):
+>>>>>>> cc794846d113c8b24da5716f928624f31d9dee47
+		return True
+	elif pygame.sprite.groupcollide(beams, players, False, False, pygame.sprite.collide_mask):
+		return True
+	else:
+		return False
+	
+def killAll():
+	enemys.empty()
+	players.empty()
+	bullets.empty()
+	beams.empty()
+	enemyBullets.empty()
+	ships.empty()
--- main.py
+++ main.py
@@ -1,84 +1,239 @@
 from Tkinter import *
 import pygame
-import sys
+from pygame import mixer
+from pygame.locals import *
+#import sys
 import pygame.sprite as sprite
+import Spriteslib as Sprites
+import shelve
+#import joystickLibv2 as Joystick
+Console = "PC"
 
-class Game(Frame):
-    def __init__(self,master):
-        Frame.__init__(self,master)
+def read_high_score():
+		s = open("score.txt", "r")
+		if (s.mode == 'r'):
+				data = s.read()
+		return data
 
-        self.startbutton = Button(master, text = "START", fg = "white",bg = "green",  command = self.play, height = 2)
-        self.startbutton.pack(side = TOP,fill = X)
+highscore = read_high_score()
 
-        self.button2 = Button(master, text = "QUIT", fg = "white",bg = "red", command = self.quit, height = 2)
-        self.button2.pack(side = TOP, fill = X)
 
-        self.img = PhotoImage(file = "space.gif")
+class Menu(Frame):
+		def __init__(self,master):
+				Frame.__init__(self,master)
 
-        self.l = Label(master, image = self.img)
-        self.l.pack(side = BOTTOM, fill=X)
+				self.startbutton = Button(master, text = "START", fg = "white",bg = "green",  command = self.CreateGame, height = 2)
+				self.startbutton.pack(side = TOP,fill = X)
 
-        
+				self.button2 = Button(master, text = "QUIT", fg = "white",bg = "red", command = self.quit, height = 2)
+				self.button2.pack(side = TOP, fill = X)
+
+				self.img = PhotoImage(file = "resources/space.gif")
+				self.highsc = Label(master, text = "HIGH SCORE : " + highscore, fg = "white", bg = "black", height = 1)
+				self.highsc.pack(side = BOTTOM, fill = X)                                
+				self.l = Label(master, image = self.img)
+				self.l.pack(side = BOTTOM, fill=X)
+	
+ 		def CreateGame(self):
+			Game(score= 0, lives= 5, waves = 0)
+
+				
+
+class Game():
+	def __init__(self, score, lives, waves):
+		pygame.init()
+		mixer.init()
+		self.laser = pygame.mixer.Sound("resources/Sounds/defaultLaser.ogg")
+		self.alienDestroyed = pygame.mixer.Sound("resources/Sounds/galaga_destroyed.ogg")
+		self.playerDestroyed = pygame.mixer.Sound("resources/Sounds/explosion.ogg")
+		self.ChannelA = pygame.mixer.Channel(0)
+		self.ChannelB = pygame.mixer.Channel(1)
+		self.ChannelC = pygame.mixer.Channel(2)
+		self.background = pygame.image.load('resources/background_image.gif')
+		self.background_size = self.background.get_size()
+		self.background_rect = self.background.get_rect()
+		self.screen = pygame.display.set_mode(self.background_size)
+		self.score = score
+		self.lives = lives
+		self.waves = waves
+		self.running = True
+		self.dead = False
+		self.play()
+
+	@property
+	def score(self):
+			return self._score
+
+	@score.setter
+	def score(self, value):
+			self._score = value
+
+	@property
+	def lives(self):
+			return self._lives
+	
+	@lives.setter
+	def lives(self, value):
+			self._lives = value
+
+	@property
+	def waves(self):
+			return self._waves
+	
+	@waves.setter
+	def waves(self, value):
+			self._waves = value
+
+
+	def events(self, Console):
+			if (Console == "PC"):
+					keys = pygame.key.get_pressed()
+
+					if keys[K_RIGHT]:
+							self.player.xVel = 5
+
+							
+					elif keys[K_LEFT]:
+							self.player.xVel = -5
+
+
+					else:
+							self.player.xVel = 0
+							self.player.yVel = 0
+
+
+					for event in pygame.event.get():
+							if event.type == pygame.QUIT:
+									global highscore
+									self.running = False
+									if (int(highscore) < self.score):
+										self.high_score(self.score)
+							if event.type == pygame.KEYDOWN:
+									if event.key==pygame.K_SPACE:
+										if self.dead == False:
+											Sprites.Bullet(self.player, self.screen)
+											self.ChannelA.play(self.laser)
+									if event.key ==pygame.K_e:
+											Sprites.Enemy1(300, 50, self.screen)
+			elif (Console == "PI"):
+					mstatus = Joystick.readChannel(1)
+					fstatus = Joystick.readChannel(0)
+					pygame.event.pump()									
+					if fstatus == "YES":
+						if self.dead == False:
+							Sprites.Bullet(self.player, self.screen)
+							self.ChannelA.play(self.laser)
+
+					if mstatus == "Right":
+							self.player.xVel = 1
+							
+					elif mstatus == "Left":
+							self.player.xVel = -1
+
+					else:
+							self.player.xVel = 0
+							self.player.yVel = 0
+
+					for event in pygame.event.get():
+							if event.type == pygame.QUIT: 
+									self.running = False
+	
+
+
+
+
 #starts the game
-    def play(self):
-        stage = Stage()
-        
-        
-#ends and exits the game
-    def quit(self):
-        self.master.destroy()
+	def play(self):
+			pygame.font.init()
+			theClock = pygame.time.Clock()
+			w,h = self.background_size
+			x = 0
+			y = 0
+			x1 = 0
+			y1 = -h
+			#Sprites.init()
+			self.player = Sprites.Player(265,430, self.screen)
+			global Console
+			self.running = True
+			while self.running:
+				#screen.blit(background,background_rect) ---- When the FPS was ramped up this was not needed and actually was the cause of our screen tearing
+				pygame.display.update()
+				y1 += 5
+				y += 5
+				self.screen.blit(self.background,(x,y))
+				self.screen.blit(self.background,(x1,y1))
+				Sprites.players.update()
+				Sprites.bullets.update()
+				Sprites.enemys.update()
+				Sprites.enemyBullets.update()
+				Sprites.beams.update()
+				Sprites.ships.update()
+				self.side_panel(self.screen)
+				self.score_counter(self.screen, self.score)
+				self.life_counter(self.screen, self.lives)
+				self.wave_counter(self.screen, self.waves)
+				self.events(Console)
+				if (len(Sprites.enemys) < 1):
+					self.waves += 1
+				Sprites.GenLevel(self.screen, self.waves)
+				self.events(Console)
+				Sprites.GenLevel(self.screen, self.waves)
+				if Sprites.enemyDeath():
+					#happens when enemy dies
+					self.score = self.score + 5
+					self.ChannelB.play(self.alienDestroyed)
+				if Sprites.playerDeath():
+					if self.lives > 0:
+						#happens when player dies but the game is not over
+						self.lives = self.lives - 1
+						self.player = Sprites.Player(250,430, self.screen)
+					else:
+						#happens when game is over
+						self.dead = True
+						self.ChannelC.play(self.playerDestroyed)
+						
+				if y > h:
+					y = -h
+				if y1 > h:
+					y1 = -h
+				pygame.display.flip()
+				pygame.display.update()
+				theClock.tick(60)
+			Sprites.killAll()
+			pygame.display.quit()
+			pygame.quit()
+
+	def side_panel(self, screen):
+		panel = pygame.Surface((150, 600))
+		panel.fill((77, 90, 142))
+		screen.blit(panel,(0,0))
+
+	def score_counter(self, screen, score):
+			myfont = pygame.font.Font("resources/Fonts/ScifiAdventure.otf", 12)
+			textsurface = myfont.render("Score: {}".format(score), False, (255, 255, 255))
+			screen.blit(textsurface,(0,0))
+
+	def life_counter(self, screen, lives):
+			myfont = pygame.font.Font("resources/Fonts/ScifiAdventure.otf", 12)
+			textsurface = myfont.render("Lives: {}".format(lives), False, (255, 255, 255))
+			screen.blit(textsurface,(0,25))
+
+	def wave_counter(self, screen, wave):
+			myfont = pygame.font.Font("resources/Fonts/ScifiAdventure.otf", 12)
+			textsurface = myfont.render("Wave: {}".format(wave), False, (255, 255, 255))
+			screen.blit(textsurface,(0,50))
 
 
-class Stage(object):
-    def __init__(self):
-        theClock = pygame.time.Clock()
+	def high_score(self, score):
+			s = open("score.txt","w+")#this opens up the file 
+			s.write(str(score))
+			s.close()
 
-        background = pygame.image.load('background_image.gif')
 
-        background_size = background.get_size()
-        background_rect = background.get_rect()
-        screen = pygame.display.set_mode(background_size)
-        w,h = background_size
-        x = 0
-        y = 0
 
-        x1 = 0
-        y1 = -h
-
-        #counter = 0
-
-        running = True
-
-        while running:
-            screen.blit(background,background_rect)
-            pygame.display.update()
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            y1 += 5
-            y += 5
-            screen.blit(background,(x,y))
-            screen.blit(background,(x1,y1))
-            if y > h:
-                y = -h
-            if y1 > h:
-                y1 = -h
-            pygame.display.flip()
-            #counter += 1
-            #score_counter(screen, "score: {}", counter)
-            pygame.display.update()
-            theClock.tick(10)
-        pygame.quit()
-
-#def score_counter(screen, score, counter):
-    #textsurface = myfont.render(score.format(counter), False, (255, 255, 255))
-    #screen.blit(textsurface,(0,0))
 
 
 #########################################################################
-#pygame.font.init()
-#myfont = pygame.font.SysFont('Comic Sans MS', 30)
-
 
 #Default window size
 WIDTH = 500
@@ -86,8 +241,12 @@ HEIGHT = 550
 window = Tk()
 window.geometry("{}x{}".format(WIDTH,HEIGHT))
 window.title("Attack of The Pi !")
-menu = Game(window)
+menu = Menu(window)
 window.mainloop()
 
 
-        
+
+
+
+
+#########################################################################
